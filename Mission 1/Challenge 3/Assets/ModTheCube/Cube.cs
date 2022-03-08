28870eefec4e3dc193b284d6dfd32d8cc6c60210
Jazer Salazar <jazersalazar@gmail.com>
2022-03-09 02:54:47 +0800

    feat: add mission 1 - challenge 3

---
+++ Mission 1/Challenge 3/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    private Color lerpedColor = Color.white;
+
+    
+    void Start()
+    {
+        float cubeScale = Random.Range(0.5f, 2f);
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * cubeScale;
+    }
+    
+    void Update()
+    {
+        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        lerpedColor = Color.Lerp(Color.black, Color.yellow, Mathf.PingPong(Time.time, 1));
+        Material material = Renderer.material;
+        material.color = lerpedColor;
+    }
+}
