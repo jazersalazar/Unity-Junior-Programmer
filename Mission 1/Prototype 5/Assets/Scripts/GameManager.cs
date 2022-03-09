8825e4e69497d2302d7e60c528b2f1f514e51e28
Jazer Salazar <jazersalazar@gmail.com>
2022-03-09 19:48:25 +0800

    feat: add game over system

--- Mission 1/Prototype 5/Assets/Scripts/GameManager.cs
+++ Mission 1/Prototype 5/Assets/Scripts/GameManager.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameManager : MonoBehaviour 
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+
+    public bool isGameActive;
 
     private float spawnRate = 1.0f;
     private int score;
@@ -14,13 +20,26 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
         UpdateScore(0);
         StartCoroutine(SpawnTarget());
     }
 
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
     IEnumerator SpawnTarget()
     {
-        while (true) {
+        while (isGameActive) {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
--- Mission 1/Prototype 5/Assets/Scripts/Target.cs
+++ Mission 1/Prototype 5/Assets/Scripts/Target.cs
@@ -45,13 +45,20 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown() 
     {
-        gameManager.UpdateScore(pointValue);
-        Destroy(gameObject);
-        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        if (gameManager.isGameActive)
+        {
+            gameManager.UpdateScore(pointValue);
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        }
     }
 
     private void OnTriggerEnter(Collider other) 
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
 }
